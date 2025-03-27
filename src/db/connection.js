30d51c0813b930e46ca8f19bfc6ad72b3396d16f
faizyi <faizi13062000@gmail.com>
2025-03-27 11:20:41 +0300

    db

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,10 @@ import { configs } from "../config/config.js";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(configs.mongoUrl);
+        await mongoose.connect(configs.mongoUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Database connected");
     } catch (error) {
         console.log("Database connection failed");
