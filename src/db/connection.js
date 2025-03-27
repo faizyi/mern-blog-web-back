3e9cb3d34831bc160815d2bb2b3fee6fb72e92fa
faizyi <faizi13062000@gmail.com>
2025-03-27 11:37:43 +0300

    dbbb

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,7 +4,7 @@ import { configs } from "../config/config.js";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://faiz557564:9G6sbBnJOCB5lsBt@cluster0.syog7.mongodb.net/?retryWrites=true&w=majority&mern-blog-web=Cluster0");
+        await mongoose.connect("mongodb+srv://faiz557564:9G6sbBnJOCB5lsBt@cluster0.syog7.mongodb.net/mern-blog-web");
         console.log("Database connected");
     } catch (error) {
         console.log("Database connection failed");
